b8b8d9ee8cdd67dda55475dd8bc85cc55de45986
wmorwaski <morwiko@gmail.com>
2024-09-16 14:11:33 +0200

    feat: add Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
