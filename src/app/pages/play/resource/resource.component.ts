a2f28fa2cfe5eb8c37383eab36056e03ffb5bbab
wmorwaski <morwiko@gmail.com>
2024-09-19 21:21:34 +0200

    feat: disable button when calculating winner

--- src/app/pages/play/resource/resource.component.ts
+++ src/app/pages/play/resource/resource.component.ts
@@ -1,30 +1,43 @@
-import {Component, DestroyRef, OnInit} from '@angular/core';
-import {ActivatedRoute, RouterLink} from '@angular/router';
-import {People, ResourceType, Starships} from '../../../types/resource.types';
-import {catchError, combineLatest, delay, EMPTY, filter, map, Subject, take, withLatestFrom} from 'rxjs';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {AsyncPipe, NgTemplateOutlet} from '@angular/common';
-import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
-import {MatButton} from '@angular/material/button';
-import {MatProgressSpinner} from '@angular/material/progress-spinner';
-import {MatList, MatListItem} from '@angular/material/list';
-import {MatDivider} from '@angular/material/divider';
-import {ResourceCardComponent} from "../../../components/resource-card/resource-card.component";
-import {MatDialog} from "@angular/material/dialog";
-import {WinnerDialogComponent} from "../../../dialogs/winner-dialog/winner-dialog.component";
-import {resourceFactory} from "../../../factories/resource/resource.factory";
-import {ResourceCreator} from "../../../factories/resource/resource.creator";
-import {Store} from "@ngxs/store";
-import {IncrementScoreAction} from "../../../scoreboard/scoreboard.actions";
-import {SWAPIService} from "../../../services/swapi.service";
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { People, ResourceType, Starships } from '../../../types/resource.types';
+import { catchError, combineLatest, delay, EMPTY, filter, map, Subject, take, withLatestFrom } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatList, MatListItem } from '@angular/material/list';
+import { MatDivider } from '@angular/material/divider';
+import { ResourceCardComponent } from '../../../components/resource-card/resource-card.component';
+import { MatDialog } from '@angular/material/dialog';
+import { WinnerDialogComponent } from '../../../dialogs/winner-dialog/winner-dialog.component';
+import { resourceFactory } from '../../../factories/resource/resource.factory';
+import { ResourceCreator } from '../../../factories/resource/resource.creator';
+import { Store } from '@ngxs/store';
+import { IncrementScoreAction } from '../../../scoreboard/scoreboard.actions';
+import { SWAPIService } from '../../../services/swapi.service';
 
 @Component({
   selector: 'app-resource',
   standalone: true,
-  imports: [AsyncPipe, MatCard, MatCardTitle, MatButton, RouterLink, MatProgressSpinner, MatCardContent, NgTemplateOutlet, MatList, MatListItem, MatDivider, ResourceCardComponent],
+  imports: [
+    AsyncPipe,
+    MatCard,
+    MatCardTitle,
+    MatButton,
+    RouterLink,
+    MatProgressSpinner,
+    MatCardContent,
+    NgTemplateOutlet,
+    MatList,
+    MatListItem,
+    MatDivider,
+    ResourceCardComponent,
+  ],
   templateUrl: './resource.component.html',
   styleUrl: './resource.component.scss',
-  providers: [{provide: ResourceCreator, useFactory: resourceFactory, deps: [ActivatedRoute]}]
+  providers: [{ provide: ResourceCreator, useFactory: resourceFactory, deps: [ActivatedRoute] }],
 })
 export class ResourceComponent implements OnInit {
   public resources$ = new Subject<[People | Starships, People | Starships] | null>();
@@ -34,6 +47,7 @@ export class ResourceComponent implements OnInit {
   public loading = false;
   public hasError = false;
   public resourceProperties: string[] = [];
+  public calculatingWinner = false;
 
   constructor(
     private SWAPIService: SWAPIService,
@@ -41,23 +55,30 @@ export class ResourceComponent implements OnInit {
     private dialog: MatDialog,
     private resourceCreator: ResourceCreator,
     private store: Store
-  ) {
-  }
+  ) {}
 
   public ngOnInit() {
     this.resourceCommonAttribute = this.resourceCreator.getCommonAttribute();
     this.resourceProperties = this.resourceCreator.getProperties();
     this.fetchResources();
-    this.winner$.pipe(filter(resource => !!resource), withLatestFrom(this.resources$), delay(1000), takeUntilDestroyed(this.destroyRef)).subscribe(([winningResource, resources]) => {
-      const winningPlayer = resources!.findIndex(resource => resource === winningResource) === 0 ? 'player1' : 'player2';
-      this.store.dispatch(new IncrementScoreAction(winningPlayer));
-      this.dialog.open(WinnerDialogComponent, {
-        data: {
-          resource: winningResource,
-          properties: this.resourceProperties,
-        }
+    this.winner$
+      .pipe(
+        filter(resource => !!resource),
+        withLatestFrom(this.resources$),
+        delay(1000),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(([winningResource, resources]) => {
+        const winningPlayer = resources!.findIndex(resource => resource === winningResource) === 0 ? 'player1' : 'player2';
+        this.calculatingWinner = false;
+        this.store.dispatch(new IncrementScoreAction(winningPlayer));
+        this.dialog.open(WinnerDialogComponent, {
+          data: {
+            resource: winningResource,
+            properties: this.resourceProperties,
+          },
+        });
       });
-    })
   }
 
   public fetchResources() {
@@ -75,11 +96,10 @@ export class ResourceComponent implements OnInit {
         })
       )
       .subscribe(([resource1, resource2]) => {
+        this.calculatingWinner = true;
         this.loading = false;
         this.hasError = false;
         this.resources$.next([resource1.result.properties, resource2.result.properties]);
       });
   }
-
-
 }
