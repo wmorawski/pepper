11fa3e414e3220aacee535a0ae66fde453595adc
wmorwaski <morwiko@gmail.com>
2024-09-19 17:32:02 +0200

    fix: fix swapi endpoint

--- src/app/pipes/key-to-label.pipe.spec.ts
+++ src/app/pipes/key-to-label.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { KeyToLabelPipe } from './key-to-label.pipe';
 
 describe('KeyToLabelPipe', () => {
+  const pipe = new KeyToLabelPipe();
+
   it('create an instance', () => {
-    const pipe = new KeyToLabelPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform underscores and capitalize first letter', () => {
+    const input = 'pipe_input';
+    const expected = 'Pipe input';
+    expect(pipe.transform(input)).toEqual(expected);
+  });
+
+  it("shouldn't transform dashes", () => {
+    const input = 'pipe-input';
+    const expected = 'Pipe-input';
+    expect(pipe.transform(input)).toEqual(expected);
   });
 });
--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,16 +1,15 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Resource, ResourceType} from "../types/resource.types";
-import {SWAPIResourceResponse} from "../types/swapi.http.types";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Resource, ResourceType } from '../types/resource.types';
+import { SWAPIResourceResponse } from '../types/swapi.http.types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SWAPIService {
-  private readonly baseUrl = 'https://swapi.dev/api/';
+  private readonly baseUrl = 'https://swapi.tech/api/';
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   public fetchResource(resource: ResourceType, id: number) {
     return this.httpClient.get<SWAPIResourceResponse<Resource>>(`${this.baseUrl}/${resource}/${id}`);
